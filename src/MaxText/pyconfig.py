38c1311f81948e818b63669393124da4a3ab163a
Qinwen Xu <qinwen@google.com>
2025-09-19 22:05:16 +0000

    update sharding to remove extra datacopy
    update
    
    refactor
    
    update
    
    Add optional config
    
    Explicitly shard input tensors across mesh devices
    
    Run on 0.7.2 candidate image
    
    Fix typo in image tag
    
    Revert to use latest tag
    
    update test for new jax version
    
    Remove sharding rules for q_lora and kv_lora from base.yml
    
    update with configs
    
    clean up
    
    update

--- src/MaxText/pyconfig.py
+++ src/MaxText/pyconfig.py
@@ -1070,6 +1070,11 @@ def validate_ring_of_experts_parallelism(raw_keys):
   if raw_keys["use_ring_of_experts"] and not using_expert_parallelism(raw_keys):
     raise ValueError("Ring-of-experts requires expert-parallelism to be enabled.")
 
+def validate_shard_fsdp_on_expert_parallelism(raw_keys):
+  if raw_keys["fsdp_shard_on_exp"] and raw_keys["num_experts"] % raw_keys["ici_fsdp_parallelism"]!=0: 
+    raise ValueError("fsdp_shard_on_exp requires num_experts is divisiable by ici_fsdp_parallelism.")
+  if raw_keys["fsdp_shard_on_exp"] and (using_tensor_parallelism(raw_keys) or useing_expert_parallelism(raw_keys)): 
+    raise ValueError("fsdp_shard_on_exp requires ici_expert_parallelism = 1 and ici_tensor_parallelism/ici_tensor_transpose_parallelism = 1.")
 
 def validate_shard_fsdp_on_expert_parallelism(raw_keys):
   if raw_keys["fsdp_shard_on_exp"] and raw_keys["num_experts"] % raw_keys["ici_fsdp_parallelism"] != 0:
